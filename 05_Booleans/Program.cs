d4dca09f23425f22633eb0ec8b371acefbf62992
Kevin Michael <kcamichael@gmail.com>
2023-05-08 07:28:22 -0400

    aion

--- 05_Booleans/Program.cs
+++ 05_Booleans/Program.cs
@@ -22,3 +22,16 @@ Console.WriteLine($"NOT False = {isTrue}");
 Console.WriteLine($"False AND True = {falseAndExample}");
 Console.WriteLine($"True OR True = {trueOrExample}");
 Console.WriteLine($"True XOR True = {falseExclusiveOr}");
+
+//-- Comparison Operators
+bool isGreaterThan = 10 > 10;
+bool isGreaterThanOrEqualTo = 10 >= 10;
+Console.WriteLine($"10 > 10 is {isGreaterThan}, and 10 >= 10 is {isGreaterThanOrEqualTo}");
+
+bool isLessThan = 5 < 5;
+bool isLessThanOrEqualTo = 5 <= 5;
+Console.WriteLine($"5 < 5 is {isLessThan}, while 5 <= 5 is {isLessThanOrEqualTo}");
+
+bool equality = 17 == 9;
+bool inequality = 17 != 9;
+Console.WriteLine($"17 == 9 and 17 != 9 are {equality} and {inequality}, respectively.");
