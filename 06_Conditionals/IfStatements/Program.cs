227ecde8f989c21bd61252309fb42f1bb9a8ed81
Kevin Michael <kcamichael@gmail.com>
2023-05-08 07:33:31 -0400

    ain

---
+++ 06_Conditionals/IfStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿bool isRaining = true;
+bool isGoingOutside = true;
+
+//if (true/false value)
+//{
+//  Body of code, execute this code if above value is true
+//}
+
+if (isRaining)
+{
+    Console.WriteLine("It's raining out!");
+}
+
+// Is raining AND is going outside
+if (isRaining && isGoingOutside)
+{
+    Console.WriteLine("Better bring an umbrella!");
+}
+
+// Not raining OR not going outside
+if (!isRaining || !isGoingOutside)
+{
+    Console.WriteLine("I do not need an umbrella.");
+}
+
+// Exclusively not raining OR going outside
+if (isRaining ^ isGoingOutside)
+{
+    Console.WriteLine("I will only go outside if it is not raining.");
+}
